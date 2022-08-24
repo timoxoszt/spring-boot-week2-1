26c3796982e2047a90885a369084a3ecfdafa0e6
timoxoszt <20520810@gm.uit.edu.vn>
2022-08-24 17:23:52 +0700

    APIController

---
+++ src/main/java/com/likelion/jpadb/controller/APIController.java
@@ -0,0 +1,9 @@
+package com.likelion.jpadb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class APIController {
+}
