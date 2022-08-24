061e805de8805e5de4e3a4161f0ba19bec7d091f
timoxoszt <20520810@gm.uit.edu.vn>
2022-08-24 18:15:21 +0700

    JpaRepository

--- src/main/java/com/likelion/jpadb/repository/EmployeeRepository.java
+++ src/main/java/com/likelion/jpadb/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.likelion.jpadb.repository;
 
-public class EmployeeRepository {
+import com.likelion.jpadb.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
