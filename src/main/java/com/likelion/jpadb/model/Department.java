ed1b94feaaf9f09bef8ac6483647c0fa7bf86ba7
timoxoszt <20520810@gm.uit.edu.vn>
2022-08-24 17:53:21 +0700

    update model

--- src/main/java/com/likelion/jpadb/model/Department.java
+++ src/main/java/com/likelion/jpadb/model/Department.java
@@ -1,4 +1,32 @@
 package com.likelion.jpadb.model;
 
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static org.hibernate.annotations.CascadeType.DELETE;
+
+@Entity
+@Table(name = "DEPARTMENT")
+@Setter
+@Getter
 public class Department {
+    @Id
+    @GeneratedValue
+    @Column(name = "departmentId", nullable = false)
+    private Long departmentId;
+
+    @Column(name = "dept_name", length = 255, nullable = false)
+    private String dept_name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "department")
+    @Cascade(value = DELETE)
+    private List<Employee> employees;
 }
--- src/main/java/com/likelion/jpadb/model/Employee.java
+++ src/main/java/com/likelion/jpadb/model/Employee.java
@@ -1,4 +1,32 @@
 package com.likelion.jpadb.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "EMPLOYEE")
+@Setter
+@Getter
 public class Employee {
+    @Id
+    @GeneratedValue
+    @Column(name = "employeeId", nullable = false)
+    private Long employeeId;
+
+    @Column(name = "name", length = 80, nullable = false)
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birthDate", nullable = false)
+    private Date birthDate;
+
+    @Column(name = "gender", nullable = false)
+    private boolean gender; // Male-1 Female-0
+
+    @ManyToOne
+    @JoinColumn(name = "departmentId")
+    private Department department;
 }
